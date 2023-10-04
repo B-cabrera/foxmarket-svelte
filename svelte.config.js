7705645acae8a9b9c45832c4c1a96cbf130ce93c
B-cabrera <brendenc29@gmail.com>
2023-10-04 13:09:26 -0400

    init: setup tailwind

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-unresolved */
+// eslint-disable-next-line import/no-extraneous-dependencies
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
