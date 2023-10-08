8e0ad516a87c1713da51ab9e77f849b5c79c26bf
B-cabrera <brendenc29@gmail.com>
2023-10-07 20:43:44 -0400

    feat: add logo in navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script>
 	import '../app.css';
+	import logo from '$lib/images/foxmarketlogo.png';
 </script>
+
+<nav class="flex h-14 items-center">
+	<a href="/">
+		<img src={logo} alt="The FoxMarket Logo" class="w-[250px] px-4 py-2" />
+	</a>
+</nav>
 
 <slot />
