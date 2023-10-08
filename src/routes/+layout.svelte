f227919c376f34ecf1830eb70feec0a8eb34c519
B-cabrera <brendenc29@gmail.com>
2023-10-07 23:16:59 -0400

    feat: add search bar w/ logo and style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,10 @@
 	<a href="/">
 		<img src={logo} alt="The FoxMarket Logo" class="w-[250px] px-4 py-2" />
 	</a>
+	<div class="flex w-3/5 items-center">
+		<i class="material-symbols-outlined text-maristgrey absolute px-2"> search </i>
+		<input type="search" class="input pl-10 tracking-wider" placeholder="Search" />
+	</div>
 </nav>
 
 <slot />
