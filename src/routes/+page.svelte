ddf0b6944590a8296516d8d6d2ffcba14e4ca360
B-cabrera <brendenc29@gmail.com>
2023-10-08 21:43:23 -0400

    feat: photo and heading to landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,12 @@
-<h1>FoxMarket</h1>
+<script lang="ts">
+	import students_photo from '$lib/images/marist-students.jpg';
+</script>
+
+<div class="relative h-1/3">
+	<img src={students_photo} class="opacity-30 w-full h-[690px]" alt="Marist students on campus." />
+	<h1
+		class="absolute text-7xl text-slate-50 top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 w-max font-semibold"
+	>
+		The Marist Student Marketplace
+	</h1>
+</div>
