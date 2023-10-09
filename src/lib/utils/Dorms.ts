6ec2d586d43055a58773ac206beff69ce0272a2f
B-cabrera <brendenc29@gmail.com>
2023-10-09 02:31:36 -0400

    feat: add dorms enum

---
+++ src/lib/utils/Dorms.ts
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+
+enum Dorms {
+	CHAMPAGNAT = 'Champagnat',
+	LEO = 'Leo',
+	MARIAN = 'Marian',
+	MIDRISE = 'Midrise',
+	SHEAHAN = 'Sheahan',
+	FOY = 'Foy',
+	FULTON = 'Fulton',
+	NEW = 'New Townhouses',
+	LOWER_CEDAR = 'Lower West Cedar',
+	UPPER_CEDAR = 'Upper West Cedar',
+	OSHEA = "O'Shea",
+	LAVELLE = 'Lavelle',
+	MCCORMICK = 'McCormick',
+	NEW_FULTON = 'New Fulton',
+	WARD = 'Ward',
+}
+
+export default Dorms;
