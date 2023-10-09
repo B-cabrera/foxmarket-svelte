7b98748d5f03aebaf5eef1ad08336bd4c043bfac
B-cabrera <brendenc29@gmail.com>
2023-10-08 20:58:38 -0400

    feat:add ButtonContainer to navbar Changed search bar width to 55%

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,19 @@
 <script>
 	import '../app.css';
 	import logo from '$lib/images/foxmarketlogo.png';
+	import ButtonContainer from '$lib/components/ButtonContainer.svelte';
 </script>
 
 <nav class="flex h-14 items-center">
 	<a href="/">
 		<img src={logo} alt="The FoxMarket Logo" class="w-[250px] px-4 py-2" />
 	</a>
-	<div class="flex w-3/5 items-center">
+	<div class="flex w-[55%] items-center">
 		<i class="material-symbols-outlined text-maristgrey absolute px-2"> search </i>
 		<input type="search" class="input pl-10 tracking-wider" placeholder="Search" />
 	</div>
+
+	<ButtonContainer />
 </nav>
 
 <slot />
