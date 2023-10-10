41adecf57f6ee93fc65511bb0f1ea4ed90396b4c
B-cabrera <brendenc29@gmail.com>
2023-10-09 20:48:57 -0400

    feat: added logo over upper photo portion

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,27 @@
 <script lang="ts">
 	import students_photo from '$lib/images/marist-students.jpg';
 	import Dorms from '$lib/utils/Dorms';
+	import logo from '$lib/images/foxmarketlogo.png';
 
 	const locations = Object.values(Dorms);
 </script>
 
 <div class="relative h-1/3">
 	<img src={students_photo} class="opacity-30 w-full h-[690px]" alt="Marist students on campus." />
+	<img
+		src={logo}
+		alt="The FoxMarket Logo"
+		class="px-4 py-2 absolute text-7xl text-slate-50 top-[15%] left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2 font-semibold"
+	/>
 	<h1
-		class="absolute text-7xl text-slate-50 top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 w-max font-semibold"
+		class="absolute text-7xl text-slate-50 top-[42%] left-1/2 -translate-x-1/2 -translate-y-1/2 w-max font-semibold"
 	>
 		The Marist Student Marketplace
 	</h1>
 
 	<a
 		href="/login"
-		class="absolute btn top-[60%] left-1/2 -translate-x-1/2 -translate-y-1/2 p-5
+		class="absolute btn top-[70%] left-1/2 -translate-x-1/2 -translate-y-1/2 p-5
     font-semibold text-2xl !border-2 text-slate-50 hover:text-maristgrey hover:border-maristgrey"
 	>
 		Shop Now
