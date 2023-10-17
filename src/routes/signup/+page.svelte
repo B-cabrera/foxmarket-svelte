3d7444c77c77e1571219d2b964580e75cc73c97b
B-cabrera <brendenc29@gmail.com>
2023-10-17 18:33:40 -0400

    fix: add input names and form submit method

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import logo from '$lib/images/foxmarketlogo.png';
 </script>
 
@@ -7,11 +7,28 @@
 		<img src={logo} alt="FoxMarket Logo" class="w-3/4 pt-6" />
 		<p class="text-slate-50 text-2xl font-semibold">Join FoxMarket!</p>
 
-		<form action="" class="flex flex-col w-2/3 gap-8 items-center">
-			<input type="text" class="input focus:border-slate-950 pl-2" placeholder="Username" />
-			<input type="email" class="input focus:border-slate-950 pl-2" placeholder="Email" />
-			<input type="password" class="input focus:border-slate-950 pl-2" placeholder="Password" />
-
+		<form method="POST" class="flex flex-col w-2/3 gap-8 items-center">
+			<input
+				name="username"
+				type="text"
+				class="input focus:border-slate-950 pl-2"
+				placeholder="Username"
+				required
+			/>
+			<input
+				name="email"
+				type="email"
+				class="input focus:border-slate-950 pl-2"
+				placeholder="Email"
+				required
+			/>
+			<input
+				name="password"
+				type="password"
+				class="input focus:border-slate-950 pl-2"
+				placeholder="Password"
+				required
+			/>
 			<button
 				class="w-1/3 bg-maristgrey border-slate-950 border-2 mt-8 text-xl font-semibold py-1 hover:opacity-80"
 			>
