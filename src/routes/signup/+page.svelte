cef7c1cc6fe7f06ce7b4dd58623f8eddfb6cd54f
B-cabrera <brendenc29@gmail.com>
2023-10-17 20:44:12 -0400

    feat: add password matching logic + error message

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
 	import logo from '$lib/images/foxmarketlogo.png';
+
+	let password1 = '';
+	let password2 = '';
+
+	$: match = password1 === password2;
 </script>
 
 <div class="grid place-items-center h-[calc(100vh-56px)]">
@@ -23,6 +28,7 @@
 				required
 			/>
 			<input
+				bind:value={password1}
 				name="password"
 				type="password"
 				class="input focus:border-slate-950 pl-2"
@@ -30,14 +36,22 @@
 				required
 			/>
 			<input
+				bind:value={password2}
 				name="password2"
 				type="password"
 				class="input focus:border-slate-950 pl-2"
 				placeholder="Confirm Password"
 				required
 			/>
+
+			<!-- Displaying message for passwords not matching. -->
+			{#if !match}
+				<p class="text-slate-50">Passwords don't match.</p>
+			{/if}
+
 			<button
-				class="w-1/3 bg-maristgrey border-slate-950 border-2 mt-8 text-xl font-semibold py-1 hover:opacity-80"
+				class="w-1/3 bg-maristgrey border-slate-950 border-2 mt-8 text-xl font-semibold py-1 hover:opacity-80 disabled:opacity-50"
+				disabled={!match}
 			>
 				Sign Up
 			</button>
