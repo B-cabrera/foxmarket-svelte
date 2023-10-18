25493ac50d18e0e3559430d112d92242cad3930f
B-cabrera <brendenc29@gmail.com>
2023-10-17 20:46:24 -0400

    style: fixed form height and sign up button margin

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <div class="grid place-items-center h-[calc(100vh-56px)]">
-	<div class="flex flex-col bg-maristred w-2/5 h-5/6 border shadow-2xl items-center gap-10">
+	<div class="flex flex-col bg-maristred w-2/5 border shadow-2xl items-center gap-10">
 		<img src={logo} alt="FoxMarket Logo" class="w-3/4 pt-6" />
 		<p class="text-slate-50 text-2xl font-semibold">Join FoxMarket!</p>
 
@@ -50,7 +50,7 @@
 			{/if}
 
 			<button
-				class="w-1/3 bg-maristgrey border-slate-950 border-2 mt-8 text-xl font-semibold py-1 hover:opacity-80 disabled:opacity-50"
+				class="w-1/3 bg-maristgrey border-slate-950 border-2 mt-8 text-xl font-semibold py-1 hover:opacity-80 disabled:opacity-50 mb-3"
 				disabled={!match}
 			>
 				Sign Up
