2600565076af696d03c32c71dccb34ffd00af1f9
B-cabrera <brendenc29@gmail.com>
2023-10-23 10:53:00 -0400

    feat: add signup post request logic in form action

--- src/routes/signup/+page.server.ts
+++ src/routes/signup/+page.server.ts
@@ -1,18 +1,22 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
-import { fail } from '@sveltejs/kit';
+import { fail, redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
 import validateSignUpInfo from '$lib/validation/signUpSchema';
+
 // eslint-disable-next-line import/prefer-default-export
 export const actions = {
-	default: async ({ request }) => {
+	default: async (event) => {
+		const { request } = event;
 		const data = await request.formData();
 
+		// get user info from form
 		const username = data.get('username') as string;
 		const email = data.get('email') as string;
 		const password = data.get('password') as string;
 		const confirmPassword = data.get('password2') as string;
 
+		// validate user input to ensure proper information
 		const result = validateSignUpInfo({
 			username,
 			email,
@@ -20,12 +24,31 @@ export const actions = {
 			confirmPassword,
 		});
 
+		// on validation failure
 		if (!result.success) {
 			return fail(400, { errors: result.errors });
 		}
 
-		return { success: true };
+		// send the sign up request
+		const response = await event.fetch('/api/users', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				username,
+				email,
+				password,
+			}),
+		});
 
-		// TODO: send sign up request
+		// send to _ on registration success (not final)
+		if (response.ok) {
+			// TODO: redirect to feed or profile page
+			throw redirect(302, '/');
+		}
+
+		// on registration failure
+		return fail(400, { errors: [(await response.json()).message] });
 	},
 } satisfies Actions;
