381fd03f50606c66cde5259981790b8526dd4525
B-cabrera <brendenc29@gmail.com>
2023-11-01 22:53:11 -0400

    feat: add form method & fields names on login form

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -6,9 +6,9 @@
 	<img src={logo} class="w-1/2" alt="FoxMarket Logo" />
 </div>
 
-<form action="" class="flex flex-col items-center mt-20 gap-8">
-	<input type="text" class="input w-1/2 !h-10 pl-4" placeholder="Username" />
-	<input type="password" class="input w-1/2 !h-10 pl-4" placeholder="Password" />
+<form method="POST" class="flex flex-col items-center mt-20 gap-8">
+	<input name="username" type="text" class="input w-1/2 !h-10 pl-4" placeholder="Username" />
+	<input name="password" type="password" class="input w-1/2 !h-10 pl-4" placeholder="Password" />
 
 	<button class="w-[20%] py-2 bg-maristred text-xl font-bold text-slate-50 hover:opacity-70"
 		>Login</button
