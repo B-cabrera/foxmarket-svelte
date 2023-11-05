171503da5299e1dde5930fed6c52afb712fd70d3
B-cabrera <brendenc29@gmail.com>
2023-11-05 00:15:35 -0400

    feat: add logout button and temp behavior

---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	async function logUserOut() {
+		console.log('Logging user out....');
+	}
+</script>
+
+<div class="flex w-screen h-screen justify-center items-center">
+	<button
+		class="btn !border-0 w-[20%] py-2 bg-maristred text-xl font-bold text-slate-50 hover:opacity-70"
+		on:click={logUserOut}
+	>
+		Logout
+	</button>
+</div>
