c05ad3b3ffba66af4f744955729c930dca69058c
B-cabrera <brendenc29@gmail.com>
2023-11-07 23:19:49 -0500

    feat: create /items api route to get all listings

---
+++ src/routes/api/items/+server.ts
@@ -0,0 +1,12 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/prefer-default-export */
+
+import { json, type RequestHandler } from '@sveltejs/kit';
+import prisma from '$lib/utils/prismaClient';
+
+export const GET: RequestHandler = async () => {
+	const allListings = await prisma.listing.findMany();
+
+	return json({ listings: allListings });
+};
