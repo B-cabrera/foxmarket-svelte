823ec6bf88f14b8f0035f01d650fef02e10965db
B-cabrera <brendenc29@gmail.com>
2023-11-07 23:46:54 -0500

    feat: add favorite button + basic functionality

--- src/lib/components/Listing.svelte
+++ src/lib/components/Listing.svelte
@@ -2,10 +2,25 @@
 	import type { Listing } from '@prisma/client';
 
 	export let listing: Listing;
+
+	function favoriteListing(event: Event) {
+		event.preventDefault();
+		console.log('Favorite');
+	}
 </script>
 
-<div class="h-80 w-60 text-slate-50">
+<div class="relative h-80 w-60 text-slate-50">
 	<a href={`/${listing.id}`} class="inline-block h-full w-full">
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<span
+			on:click={favoriteListing}
+			tabindex="0"
+			role="button"
+			class="absolute text-maristred font-bold material-symbols-outlined right-0 hover:cursor-pointer"
+		>
+			favorite
+		</span>
+
 		<img src={listing.imageUrl} alt="" />
 		<div class="flex pt-2 justify-between">
 			<p>{listing.listingTitle}</p>
