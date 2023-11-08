d3cbebb380eb0c81755b2bfa1345d98f2b7a5b36
B-cabrera <brendenc29@gmail.com>
2023-11-07 23:23:05 -0500

    feat: create listing component to show on feed

---
+++ src/lib/components/Listing.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { Listing } from '@prisma/client';
+
+	export let listing: Listing;
+</script>
+
+<div class="h-80 w-60 text-slate-50">
+	<a href={`/${listing.id}`} class="inline-block h-full w-full">
+		<img src={listing.imageUrl} alt="" />
+		<div class="flex pt-2 justify-between">
+			<p>{listing.listingTitle}</p>
+			<b>{`$ ${listing.price}`}</b>
+		</div>
+
+		<div class="pt-2">
+			<p>{listing.location}</p>
+		</div>
+	</a>
+</div>
