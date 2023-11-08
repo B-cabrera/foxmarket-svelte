f2f859ffd452ff03ca3436c6dfcf4b0704d92d09
B-cabrera <brendenc29@gmail.com>
2023-11-07 23:23:27 -0500

    feat: use load data to render listings

--- src/routes/feed/+page.svelte
+++ src/routes/feed/+page.svelte
@@ -1,7 +1,15 @@
-<script>
+<script lang="ts">
+	import Listing from '$lib/components/Listing.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
 </script>
 
 <div id="feed" class="h-[calc(100vh-56px)] flex">
 	<div id="filters" class="w-1/6">Filters</div>
-	<div id="items" class="w-full">Feed</div>
+	<div id="items" class="w-full">
+		{#each data.listings as listing}
+			<Listing {listing} />
+		{/each}
+	</div>
 </div>
