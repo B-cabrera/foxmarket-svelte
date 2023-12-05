ea3e913bfb4ac90cce4d27ff6cc904f27c35d314
B-cabrera <brendenc29@gmail.com>
2023-12-04 21:41:02 -0500

    feat: add server load func to items page

---
+++ src/routes/items/+page.server.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/prefer-default-export */
+
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ fetch }) => {
+	// TODO: return all the listings that belong to this user
+};
