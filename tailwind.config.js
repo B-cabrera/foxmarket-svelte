3ef858e4109b0541bb30862f246515a963022d2e
B-cabrera <brendenc29@gmail.com>
2023-12-17 23:55:25 -0500

    fix: update tailwind config for skeleton styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 import { skeleton } from '@skeletonlabs/tw-plugin';
+import { join } from 'path';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}',
+		join(
+			require.resolve(
+				'@skeletonlabs/skeleton',
+			),
+			'../**/*.{html,js,svelte,ts}',
+		)],
 	theme: {
 		extend: {
 			colors: {
