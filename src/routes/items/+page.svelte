12ac6ebe7937427d286f0a4a7200e6ed80fb96c7
B-cabrera <brendenc29@gmail.com>
2023-12-17 22:29:45 -0500

    feat: add search functionality for user items

--- src/routes/items/+page.svelte
+++ src/routes/items/+page.svelte
@@ -1,12 +1,38 @@
 <script lang="ts">
 	import Listing from '$lib/components/Listing.svelte';
+	import type { Listing as TListing } from '@prisma/client';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+	const originalListings = data.listings as TListing[];
+	let filteredListings: TListing[] = originalListings;
+	let searchQuery = '';
+
+	function filterListings() {
+		filteredListings = originalListings.filter((item) => {
+			return item.listingTitle.toLowerCase().includes(searchQuery.trim().toLowerCase());
+		});
+	}
+
+	$: {
+		const dependency = searchQuery;
+
+		filterListings();
+	}
 </script>
 
 <div class="flex flex-col items-center h-screen w-full gap-2">
-	{#each data.listings as listing}
-		<Listing {listing} display="row" />
+	<div class="flex w-[55%] items-center">
+		<i class="material-symbols-outlined text-maristgrey absolute px-2"> search </i>
+		<input
+			type="search"
+			class="input pl-10 tracking-wider font-bold"
+			placeholder="Search"
+			bind:value={searchQuery}
+		/>
+	</div>
+
+	{#each filteredListings as item (item.id)}
+		<Listing listing={item} display="row" />
 	{/each}
 </div>
