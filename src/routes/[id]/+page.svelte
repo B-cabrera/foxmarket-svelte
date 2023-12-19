8d3b7e71aa2eb4cd82deea0f93541d16bfab96af
B-cabrera <brendenc29@gmail.com>
2023-12-19 15:10:56 -0500

    fix: fix item page look on laptop view

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -8,13 +8,13 @@
 </script>
 
 <div class="flex w-full h-[calc(100vh-56px)]">
-	<div class="w-2/3 grid place-items-center">
+	<div class="w-3/5 grid place-items-center">
 		<img class="w-4/6" src={item.imageUrl} alt="" />
 	</div>
 
-	<div class="flex items-center w-1/3">
+	<div class="flex items-center justify-center w-2/5">
 		<div class="flex flex-col items-center h-3/4 w-4/6">
-			<div class="text-slate-50">
+			<div class="text-slate-50 w-max max-w-[120%]">
 				<h1 class="font-bold text-5xl tracking-wider">{item.listingTitle}</h1>
 				<span class="flex justify-between">
 					<p>{item.location}</p>
@@ -37,14 +37,12 @@
 				</button>
 			</div>
 
-			<div class="pt-10 text-slate-50 tracking-wide">
-				<h1 class="text-xl font-bold">Description</h1>
-
-				<p class="grid place-items-center h-32 text-2xl text-center">
-					{item.description}
+			<div class="text-slate-50 tracking-wide">
+				<p class="grid place-items-center h-1/2 text-2xl text-center">
+					"{item.description}"
 				</p>
 
-				<h1 class="pt-10 text-xl font-bold">Sold By</h1>
+				<h1 class="text-xl font-bold">Sold By</h1>
 
 				<div
 					class="flex items-center p-4 bg-maristgrey h-32 text-slate-950 font-bold justify-around text-xl"
