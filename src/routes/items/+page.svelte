41a8004119edfb62f60c0dc4d2fb89f3cd8ab288
B-cabrera <brendenc29@gmail.com>
2023-12-19 18:08:17 -0500

    feat: add create listing btn to trigger modal

--- src/routes/items/+page.svelte
+++ src/routes/items/+page.svelte
@@ -2,7 +2,20 @@
 	import Listing from '$lib/components/Listing.svelte';
 	import type { Listing as TListing } from '@prisma/client';
 	import type { PageData } from './$types';
-	import { SlideToggle } from '@skeletonlabs/skeleton';
+	import {
+		SlideToggle,
+		getModalStore,
+		type ModalSettings,
+		type ModalComponent,
+	} from '@skeletonlabs/skeleton';
+	import CreateListingModal from '$lib/components/CreateListingModal.svelte';
+
+	const modalStore = getModalStore();
+	const modalComponent: ModalComponent = { ref: CreateListingModal };
+	const modal: ModalSettings = {
+		type: 'component',
+		component: modalComponent,
+	};
 
 	export let data: PageData;
 	const originalListings = data.listings as TListing[];
@@ -30,7 +43,7 @@
 <div class="flex flex-col items-center h-screen w-full gap-2">
 	<h1 class="text-5xl text-slate-50 py-6 font-bold tracking-wider">My Items</h1>
 
-	<div class="flex w-[55%] items-center gap-40">
+	<div class="flex w-[55%] items-center gap-10">
 		<i class="material-symbols-outlined text-maristgrey absolute px-2"> search </i>
 		<input
 			type="search"
@@ -49,6 +62,13 @@
 				class="bg-maristgrey"
 			/>
 		</div>
+
+		<button
+			class="btn bg-maristred text-slate-50 text-xl borderround"
+			on:click={() => modalStore.trigger(modal)}
+		>
+			Create Listing
+		</button>
 	</div>
 
 	{#each filteredListings as item (item.id)}
