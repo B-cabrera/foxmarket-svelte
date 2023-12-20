e78bdcc068ae3243c8c0882a492b41b71cc872ad
B-cabrera <brendenc29@gmail.com>
2023-12-19 19:06:30 -0500

    feat: add file upload component to listing form

--- src/lib/components/CreateListingModal.svelte
+++ src/lib/components/CreateListingModal.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import Dorms from '$lib/utils/Dorms';
-	import { getModalStore } from '@skeletonlabs/skeleton';
+	import { FileDropzone, getModalStore } from '@skeletonlabs/skeleton';
 
 	const modalStore = getModalStore();
+	let file: FileList;
 </script>
 
-<div class="flex flex-col bg-maristred w-1/2 h-[60vh] border items-center justify-center">
+<div class="flex flex-col bg-maristred w-1/2 h-max border items-center justify-center py-10">
 	<form method="POST" class="flex flex-col w-2/3 gap-8 items-center">
 		<input
 			name="title"
@@ -55,5 +56,29 @@
 				<option value="dorm">{dorm}</option>
 			{/each}
 		</select>
+
+		<FileDropzone
+			name="files"
+			class="text-slate-50"
+			accept="image/jpg, image/png, image/jpeg, image/heic"
+			bind:files={file}
+			required
+		>
+			<svelte:fragment slot="lead">
+				{#if !file}
+					<span class="material-symbols-outlined text-5xl"> upload_file </span>
+				{:else}
+					<span class="material-symbols-outlined text-5xl"> image </span>
+				{/if}
+			</svelte:fragment>
+
+			<svelte:fragment slot="message">
+				{#if !file}
+					<p class="font-bold tracking-wide">Upload or drap and drop a photo of your item!.</p>
+				{:else}
+					<p class="font-bold tracking-wide text-xl">{file.item(0)?.name}</p>
+				{/if}
+			</svelte:fragment>
+		</FileDropzone>
 	</form>
 </div>
