fe8c0d43049b2e23eeab20d2b7681d42924f50d3
B-cabrera <brendenc29@gmail.com>
2024-01-28 22:55:53 -0500

    feat: have logo link direct to feed if authed and home if not

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,7 @@
 </script>
 
 <nav class="sticky flex h-14 items-center top-0 bg-maristdarkgrey z-10">
-	<a href="/">
+	<a href={showAuthedButtons ? '/feed' : '/'}>
 		<img src={logo} alt="The FoxMarket Logo" class="w-[250px] px-4 py-2" />
 	</a>
 	<div class="flex w-[55%] items-center">
