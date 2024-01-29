8d1bc8a76d11c5e3340ea2f76f10cadb864e1a7b
B-cabrera <brendenc29@gmail.com>
2024-01-28 22:07:20 -0500

    fix: remove error locals use + more error handling and redirects on errors

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,52 +1,59 @@
+/* eslint-disable no-console */
 /* eslint-disable import/extensions */
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable no-param-reassign */
 /* eslint-disable import/no-unresolved */
 
-import type { Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 import supabaseClient from '$lib/utils/supabaseClient';
+import checkValidRerouteLocation from '$lib/utils/utils';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const accessToken = event.cookies.get('access');
 	const refreshToken = event.cookies.get('refresh');
+	const shouldRedirect = checkValidRerouteLocation(event.url.pathname);
 
-	try {
-		// check if is user is authorized
-		if (refreshToken && accessToken) {
-			await supabaseClient.auth.setSession({
-				access_token: accessToken,
-				refresh_token: refreshToken,
-			});
-		} else {
-			// user is not authorized
-			event.locals.error = { status: 400, message: 'User is not authorized' };
+	// Check for invalid cookies initially
+	if (!refreshToken || !accessToken) {
+		// user is not authorized
 
-			const response = await resolve(event);
-			return response;
-		}
-
-		const { error, data } = await supabaseClient.auth.getSession();
-
-		if (error) {
-			console.error(error);
-
-			event.locals.error = { status: 500, message: 'Error getting user' };
-
-			const response = await resolve(event);
-			return response;
-		}
-
-		// User is authorized
-		event.locals.data = { loggedIn: true, userID: data.session?.user.id };
-
-		const response = await resolve(event);
-		return response;
-	} catch (error) {
-		const err = error as Error;
-
-		event.locals.error = { status: 500, message: err.message };
+		if (shouldRedirect) throw redirect(303, '/login');
 
 		const response = await resolve(event);
 		return response;
 	}
+
+	// try setting session (won't work if cookies are invalid)
+	const { error } = await supabaseClient.auth.setSession({
+		access_token: accessToken,
+		refresh_token: refreshToken,
+	});
+
+	// on setting session error
+	if (error) {
+		console.error(error.message);
+
+		if (shouldRedirect) throw redirect(303, '/login');
+
+		const response = await resolve(event);
+		return response;
+	}
+
+	// retrieving session data to verify user is authed
+	const { error: fail, data } = await supabaseClient.auth.getSession();
+
+	if (fail) {
+		console.error(fail);
+
+		if (shouldRedirect) throw redirect(303, '/login');
+
+		const response = await resolve(event);
+		return response;
+	}
+
+	// User is authorized`
+	event.locals.data = { loggedIn: true, userID: data.session?.user.id };
+
+	const response = await resolve(event);
+	return response;
 };
