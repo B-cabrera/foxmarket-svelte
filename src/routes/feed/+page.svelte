8b0a18aeddab77b10147d87b5bb6e0ee189a97ce
B-cabrera <brendenc29@gmail.com>
2024-02-26 01:23:36 -0500

    feat: pass userId to listing component

--- src/routes/feed/+page.svelte
+++ src/routes/feed/+page.svelte
@@ -3,13 +3,14 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+	const userID = data.userID!;
 </script>
 
 <div id="feed" class="h-[calc(100vh-56px)] flex">
 	<div id="filters" class="w-1/6">Filters</div>
 	<div id="items" class="w-full grid grid-cols-4 gap-8 px-5">
 		{#each data.listings as listing}
-			<Listing {listing} />
+			<Listing {listing} {userID} />
 		{/each}
 	</div>
 </div>
