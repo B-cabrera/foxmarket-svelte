fa6accf0afc6435997194c1023804dc7fb17116d
B-cabrera <brendenc29@gmail.com>
2024-02-29 14:35:10 -0500

    feat: add favorite form action to handle favorite posts

--- src/routes/feed/+page.server.ts
+++ src/routes/feed/+page.server.ts
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/prefer-default-export */
 
 import type { Listing } from '@prisma/client';
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ fetch }) => {
 	const data = await fetch('/api/items');
@@ -13,3 +14,14 @@ export const load: PageServerLoad = async ({ fetch }) => {
 		listings: listings as Listing[],
 	};
 };
+
+export const actions = {
+	favorite: async ({ request, locals }) => {
+		const data = await request.formData();
+		const listingToFavorite = data.get('listing_id') as string;
+		const favoritingUser = locals.data?.userID!;
+
+		// TODO: call favorite API here
+		console.log(listingToFavorite, favoritingUser); // temp, will remove
+	},
+} satisfies Actions;
