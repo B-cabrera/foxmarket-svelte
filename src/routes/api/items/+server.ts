15f7e67a3f24d57058ef30f8ceb3a2e1f527736c
B-cabrera <brendenc29@gmail.com>
2024-02-29 01:14:44 -0500

    feat: add bool for isfavorited in items get

--- src/routes/api/items/+server.ts
+++ src/routes/api/items/+server.ts
@@ -5,10 +5,24 @@
 import { json, type RequestHandler } from '@sveltejs/kit';
 import prisma from '$lib/utils/prismaClient';
 
-export const GET: RequestHandler = async () => {
+export const GET: RequestHandler = async ({ locals }) => {
 	const allListings = await prisma.listing.findMany({
 		where: { sold: false },
+		include: {
+			favoritedBy: true,
+		},
 	});
 
-	return json({ listings: allListings });
+	const listingsWithFavBool = allListings.map((listing) => {
+		const isFavoritedByCurrentUser = listing.favoritedBy.some(
+			(favorite) => favorite.userId.toString() === locals.data?.userID,
+		);
+
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		const { favoritedBy, ...restOfListing } = listing;
+
+		return { ...restOfListing, isFavoritedByCurrentUser };
+	});
+
+	return json({ listings: listingsWithFavBool });
 };
