60a504d30095eeef2ea01e61cbcd55c5bad7ce97
B-cabrera <brendenc29@gmail.com>
2024-03-01 00:53:29 -0500

    feat: create endpoint to favorite listing

---
+++ src/routes/api/favorite/+server.ts
@@ -0,0 +1,35 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/prefer-default-export */
+
+import { error, json, type RequestHandler } from '@sveltejs/kit';
+import type { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import prisma from '$lib/utils/prismaClient';
+
+export const POST: RequestHandler = async ({ request }) => {
+	const { listingID, favoritingUser } = await request.json();
+
+	try {
+		await prisma.favorite.create({
+			data: {
+				userId: favoritingUser,
+				listingId: listingID,
+			},
+		});
+	} catch (err) {
+		const typedError = err as PrismaClientKnownRequestError;
+
+		// user has already favorite the post
+		if (typedError.code === 'P2002') {
+			throw error(409, {
+				message: 'User has already favorited this listing.',
+			});
+		}
+
+		throw error(500, {
+			message: typedError.message,
+		});
+	}
+
+	return json({});
+};
