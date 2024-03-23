2400ce6d80af2fc56598a9268e95de42250465ff
B-cabrera <brendenc29@gmail.com>
2024-03-23 00:03:19 -0400

    feat: change submit name to save

--- src/lib/components/EditForm.svelte
+++ src/lib/components/EditForm.svelte
@@ -142,7 +142,7 @@
 				class="w-1/3 bg-maristred border-slate-50 border-2 mt-8 text-xl text-slate-50 py-1 hover:opacity-80 disabled:opacity-50 mb-3"
 				disabled={isLoading}
 			>
-				{isLoading ? 'Submitting...' : 'List Item'}
+				{isLoading ? 'Saving...' : 'Save'}
 			</button>
 			<button
 				class="w-1/3 bg-maristgrey border-slate-950 border-2 mt-8 text-xl text-slate-950 py-1 hover:opacity-80 disabled:opacity-50 mb-3"
