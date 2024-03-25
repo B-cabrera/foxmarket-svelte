f03be84c6bed5f9658ae0f3eb330845f240af485
B-cabrera <brendenc29@gmail.com>
2024-03-25 18:57:52 -0400

    feat: use toggle and chat lists for message feed

--- src/routes/chats/+page.svelte
+++ src/routes/chats/+page.svelte
@@ -2,68 +2,40 @@
 	import MessageInput from '$lib/components/MessageInput.svelte';
 	import MessageFeedBlock from '$lib/components/MessageFeedBlock.svelte';
 	import TwoButtonToggle from '$lib/components/TwoButtonToggle.svelte';
+	import type { PageData } from './$types';
+	import type { ChatInformation } from './+page.server';
+
+	export let data: PageData;
+	const { buyingChats, sellingChats } = data;
 
 	let currentMessage = '';
-
-	// TEMP SAMPLE MESSAGE DATA, STRUCTURE CAN CHANGE
-	let messageFeed = [
-		{
-			isCurrentUser: true,
-			name: 'Jane',
-			message: 'HEY.',
-		},
-		{
-			isCurrentUser: false,
-			name: 'Michael',
-			message: 'yo',
-		},
-		{
-			isCurrentUser: true,
-			name: 'Jane',
-			message: 'how are you',
-		},
-		{
-			isCurrentUser: false,
-			name: 'Michael',
-			message: 'good',
-		},
-		{
-			isCurrentUser: true,
-			name: 'Jane',
-			message: 'nice',
-		},
-		{
-			isCurrentUser: false,
-			name: 'Michael',
-			message: 'yep',
-		},
-		{
-			isCurrentUser: true,
-			name: 'Jane',
-			message: 'i needa go',
-		},
-		{
-			isCurrentUser: false,
-			name: 'Michael',
-			message: 'okay bye',
-		},
-		{
-			isCurrentUser: true,
-			name: 'Michael',
-			message: 'say less',
-		},
-	];
-
 	let isBuyingActive = true;
+	let activeChatIndex = 0;
+	let activeChat: ChatInformation;
+	const userID = data.userID!;
+
+	$: {
+		if (isBuyingActive) {
+			activeChatIndex = activeChatIndex >= buyingChats.length ? 0 : activeChatIndex;
+			activeChat = buyingChats[activeChatIndex];
+		} else {
+			activeChatIndex = activeChatIndex >= sellingChats.length ? 0 : activeChatIndex;
+			activeChat = sellingChats[activeChatIndex];
+		}
+	}
 </script>
 
 <div class="h-[calc(100vh-56px)] w-full grid grid-cols-[250px_1fr]">
-	<div class="flex flex-col items-center border p-8 pt-0">
+	<div class="flex flex-col items-center border pt-0">
 		<TwoButtonToggle bind:isLeftActive={isBuyingActive} />
 	</div>
 	<div class="border">
 		<div class="h-full grid grid-rows-[1fr_auto]">
-			<MessageFeedBlock {messageFeed} />
+			<MessageFeedBlock
+				otherUserName={isBuyingActive ? activeChat.seller?.username : activeChat.buyer?.username}
+				messageFeed={activeChat.Message}
+				currentUserID={userID}
+			/>
 			<MessageInput {currentMessage} />
 		</div>
 	</div>
