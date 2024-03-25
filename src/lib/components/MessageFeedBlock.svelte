e1166e82c15ec9ce0fa6d61a2964978da3e7de6f
B-cabrera <brendenc29@gmail.com>
2024-03-25 09:17:50 -0400

    feat: create message feed block cmpnt

---
+++ src/lib/components/MessageFeedBlock.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import MessageRow from './MessageRow.svelte';
+
+	export let messageFeed: {
+		isCurrentUser: boolean;
+		name: string;
+		message: string;
+	}[];
+</script>
+
+<div class=" p-4 overflow-y-auto h-full">
+	<section class="w-full max-h-[82vh] overflow-y-auto space-y-4">
+		{#each messageFeed as chat}
+			<MessageRow
+				senderName={chat.name}
+				message={chat.message}
+				isSentFromCurrentUser={chat.isCurrentUser}
+			/>
+		{/each}
+	</section>
+</div>
