ad1d91862c2f6555e1131a290225d9d478f7af5c
B-cabrera <brendenc29@gmail.com>
2024-03-25 15:52:43 -0400

    feat: use button toggle on chat side

--- src/routes/chats/+page.svelte
+++ src/routes/chats/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import MessageInput from '$lib/components/MessageInput.svelte';
 	import MessageFeedBlock from '$lib/components/MessageFeedBlock.svelte';
+	import TwoButtonToggle from '$lib/components/TwoButtonToggle.svelte';
 
 	let currentMessage = '';
 
@@ -52,10 +53,14 @@
 			message: 'say less',
 		},
 	];
+
+	let isBuyingActive = true;
 </script>
 
-<div class="h-[calc(100vh-56px)] w-full grid grid-cols-[200px_1fr]">
-	<div class="border bg-maristred p-8">Chats Here</div>
+<div class="h-[calc(100vh-56px)] w-full grid grid-cols-[250px_1fr]">
+	<div class="flex flex-col items-center border p-8 pt-0">
+		<TwoButtonToggle bind:isLeftActive={isBuyingActive} />
+	</div>
 	<div class="border">
 		<div class="h-full grid grid-rows-[1fr_auto]">
 			<MessageFeedBlock {messageFeed} />
