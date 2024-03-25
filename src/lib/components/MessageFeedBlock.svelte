7b3627087a7077e227711329a1f4c64816de6023
B-cabrera <brendenc29@gmail.com>
2024-03-25 18:54:48 -0400

    fix: use updated types for message feed

--- src/lib/components/MessageFeedBlock.svelte
+++ src/lib/components/MessageFeedBlock.svelte
@@ -2,19 +2,26 @@
 	import MessageRow from './MessageRow.svelte';
 
 	export let messageFeed: {
-		isCurrentUser: boolean;
-		name: string;
-		message: string;
+		id: string;
+		conversationId: string;
+		timeSent: Date;
+		senderId: string;
+		receiverId: string;
+		content: string;
 	}[];
+	export let otherUserName: string | undefined;
+	export let currentUserID: string;
+
+	$: senderName = otherUserName!;
 </script>
 
 <div class=" p-4 overflow-y-auto h-full">
 	<section class="w-full max-h-[82vh] overflow-y-auto space-y-4">
 		{#each messageFeed as chat}
 			<MessageRow
-				senderName={chat.name}
-				message={chat.message}
-				isSentFromCurrentUser={chat.isCurrentUser}
+				{senderName}
+				message={chat.content}
+				isSentFromCurrentUser={currentUserID == chat.senderId}
 			/>
 		{/each}
 	</section>
