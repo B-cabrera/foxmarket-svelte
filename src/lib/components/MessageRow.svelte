56010a2438adfa98535176be5221ca713ce4bfb8
B-cabrera <brendenc29@gmail.com>
2024-03-25 00:32:15 -0400

    feat: create message row cmpnt

---
+++ src/lib/components/MessageRow.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import MessageBubble from './MessageBubble.svelte';
+
+	export let senderName: string;
+	export let message: string;
+	export let isSentFromCurrentUser: boolean;
+
+	const messageType = isSentFromCurrentUser ? 'sender' : 'reciever';
+</script>
+
+<div class={`grid ${isSentFromCurrentUser ? 'grid-cols-[1fr_400px]' : 'grid-cols-[400px_1fr]'}`}>
+	{#if isSentFromCurrentUser}
+		<div />
+		<MessageBubble {senderName} {message} {messageType} />
+	{:else}
+		<MessageBubble {senderName} {message} {messageType} />
+		<div />
+	{/if}
+</div>
