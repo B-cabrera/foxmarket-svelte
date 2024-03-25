e63967279bf8f1265c4292f3ef30d40775a12575
B-cabrera <brendenc29@gmail.com>
2024-03-25 18:50:19 -0400

    feat: add base route for getting chats

---
+++ src/routes/api/chats/[user_id]/+server.ts
@@ -0,0 +1,61 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/prefer-default-export */
+
+import { json } from '@sveltejs/kit';
+import prisma from '$lib/utils/prismaClient';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async ({ params }) => {
+	const userID = params.user_id;
+
+	const buyingChats = await prisma.chat.findMany({
+		where: {
+			buyerId: userID,
+		},
+		include: {
+			Message: {
+				orderBy: {
+					timeSent: 'asc',
+				},
+			},
+			seller: {
+				select: {
+					username: true,
+				},
+			},
+			item: {
+				select: {
+					listingTitle: true,
+					imageUrl: true,
+				},
+			},
+		},
+	});
+
+	const sellingChats = await prisma.chat.findMany({
+		where: {
+			sellerId: userID,
+		},
+		include: {
+			Message: {
+				orderBy: {
+					timeSent: 'asc',
+				},
+			},
+			buyer: {
+				select: {
+					username: true,
+				},
+			},
+			item: {
+				select: {
+					listingTitle: true,
+					imageUrl: true,
+				},
+			},
+		},
+	});
+
+	return json({ buyingChats, sellingChats });
+};
