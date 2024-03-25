af4a83bcbd565d45f0ef4dcf27b51ecc1b5b2649
B-cabrera <brendenc29@gmail.com>
2024-03-25 08:53:54 -0400

    feat: create message input cmpnt

---
+++ src/lib/components/MessageInput.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let currentMessage: string;
+</script>
+
+<div class="p-2">
+	<div class="flex justify-center grid-cols-[1fr_auto]">
+		<textarea
+			bind:value={currentMessage}
+			class="input pl-2 tracking-wider font-bold resize-none pb-0 pt-[2px]"
+			placeholder="Write a message..."
+			rows="1"
+		/>
+		<button class="btn py-2 bg-maristred text-slate-50 font-bold hover:opacity-70 w-1/6"
+			>Send</button
+		>
+	</div>
+</div>
