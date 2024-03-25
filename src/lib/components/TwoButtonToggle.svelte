49f3d659159aca02889dc4fa9084739361bd3814
B-cabrera <brendenc29@gmail.com>
2024-03-25 15:42:38 -0400

    feat: create button toggle cmpnt

---
+++ src/lib/components/TwoButtonToggle.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let isLeftActive: boolean = true;
+
+	const activeButtonStyles = 'w-2/3 bg-maristred text-slate-50';
+	const inactiveButtonStyles = 'w-1/3 bg-maristgrey text-slate-900';
+</script>
+
+<span class="flex w-[250px]">
+	<button
+		class={`btn ${isLeftActive ? activeButtonStyles : inactiveButtonStyles} `}
+		on:click={() => (isLeftActive = true)}>Buying</button
+	>
+	<button
+		class={`btn ${isLeftActive ? inactiveButtonStyles : activeButtonStyles} `}
+		on:click={() => (isLeftActive = false)}>Selling</button
+	>
+</span>
