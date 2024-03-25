5af3b8477a56ff6c7d08cf712a3e546ce12a008a
B-cabrera <brendenc29@gmail.com>
2024-03-25 18:50:38 -0400

    feat: add chats fetch in server load

--- src/routes/chats/+page.server.ts
+++ src/routes/chats/+page.server.ts
@@ -1,7 +1,36 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
-/* eslint-disable import/prefer-default-export */
 
+import type { Chat } from '@prisma/client';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async () => {};
+export interface ChatInformation extends Chat {
+	item: {
+		listingTitle: string;
+		imageUrl: string;
+	};
+	seller?: {
+		username: string;
+	};
+	buyer?: {
+		username: string;
+	};
+	Message: {
+		id: string;
+		conversationId: string;
+		timeSent: Date;
+		senderId: string;
+		receiverId: string;
+		content: string;
+	}[];
+}
+
+export const load: PageServerLoad = async ({ fetch, locals }) => {
+	const data = await fetch(`/api/chats/${locals.data?.userID}`);
+	const { buyingChats, sellingChats } = await data.json();
+
+	return {
+		buyingChats: buyingChats as ChatInformation[],
+		sellingChats: sellingChats as ChatInformation[],
+	};
+};
