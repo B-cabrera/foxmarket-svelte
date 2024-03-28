8ccd937f51f804ef803ea6b4ed11f9ed0f6f5ac9
B-cabrera <brendenc29@gmail.com>
2024-03-28 13:33:47 -0400

    feat: add send logic for client messages

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,42 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 /* eslint-disable import/prefer-default-export */
+import type { Message } from '@prisma/client';
 import type { LayoutServerLoad } from './$types';
+import supabaseClient from '$lib/utils/supabaseClient';
 
-export const load: LayoutServerLoad = async ({ locals }) => ({
-	userID: locals.data?.userID,
-});
+let isListenerSetup = false;
+
+export const load: LayoutServerLoad = async ({ locals }) => {
+	const userID = locals.data?.userID;
+
+	if (isListenerSetup || userID === undefined) return { userID };
+
+	supabaseClient
+		.channel(userID)
+		.on('broadcast', { event: 'outMessage' }, async (payload) => {
+			const newMessage = payload.payload.newMessage.message as Message;
+			const receiverChannel = supabaseClient.channel(newMessage.receiverId);
+
+			// send message to the reciever
+			receiverChannel.subscribe(async (status) => {
+				if (status === 'SUBSCRIBED') {
+					const result = await receiverChannel.send({
+						type: 'broadcast',
+						event: 'inMessage',
+						payload: {
+							newMessage,
+						},
+					});
+
+					// HANDLE SEND ERROR HERE
+					console.log(result);
+				}
+			});
+		})
+		.subscribe();
+
+	isListenerSetup = true;
+
+	return { userID };
+};
