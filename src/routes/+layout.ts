b9ea8e478653743a8683de77379bfaef816e32a2
B-cabrera <brendenc29@gmail.com>
2024-03-28 13:27:18 -0400

    feat: add realtime channel subscribe in layout

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -8,6 +8,11 @@ import { PUBLIC_SUPABASE_ANON, PUBLIC_SUPABASE_URL } from '$env/static/public';
 
 export const load = (async ({ data }) => {
 	const supabaseBrowserClient = createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON);
+	const userChannel = supabaseBrowserClient.channel(data.userID!).subscribe();
 
-	return { ...data, supabaseBrowserClient };
+	return {
+		...data,
+		supabaseBrowserClient,
+		userChannel,
+	};
 }) satisfies LayoutLoad;
