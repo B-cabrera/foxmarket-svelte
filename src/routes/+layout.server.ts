e55a5781fe7b8591cbd1afca78b29f41fb79f57b
B-cabrera <brendenc29@gmail.com>
2024-03-30 15:39:48 -0400

    feat: add message batch logic w/ timer

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/prefer-default-export */
@@ -6,6 +8,9 @@ import type { LayoutServerLoad } from './$types';
 import supabaseClient from '$lib/utils/supabaseClient';
 
 let isListenerSetup = false;
+let currentTimer: NodeJS.Timeout | null = null;
+const messageBatch = new Map<string, Message[]>();
+const MESSAGE_WAIT_MS = 500;
 
 export const load: LayoutServerLoad = async ({ locals }) => {
 	const userID = locals.data?.userID;
@@ -18,21 +23,38 @@ export const load: LayoutServerLoad = async ({ locals }) => {
 			const newMessage = payload.payload.newMessage as Message;
 			const receiverChannel = supabaseClient.channel(newMessage.receiverId);
 
-			// send message to the reciever
-			receiverChannel.subscribe(async (status) => {
-				if (status === 'SUBSCRIBED') {
-					const result = await receiverChannel.send({
-						type: 'broadcast',
-						event: 'inMessage',
-						payload: {
-							newMessage,
-						},
-					});
+			if (currentTimer) {
+				clearTimeout(currentTimer);
+			}
 
-					// HANDLE SEND ERROR HERE
-					console.log(result);
-				}
-			});
+			if (!messageBatch.has(newMessage.conversationId)) {
+				messageBatch.set(newMessage.conversationId, []);
+			}
+
+			messageBatch.get(newMessage.conversationId)?.push(newMessage);
+
+			currentTimer = setTimeout(() => {
+				receiverChannel.subscribe(async (status) => {
+					if (status === 'SUBSCRIBED') {
+						const result = await receiverChannel.send({
+							type: 'broadcast',
+							event: 'inMessage',
+							payload: {
+								messages: Array.from(messageBatch.entries()),
+							},
+						});
+
+						// reset batch after sending
+						messageBatch.clear();
+						currentTimer = null;
+
+						// HANDLE SEND ERROR HERE
+						console.log(result);
+					}
+				});
+			}, MESSAGE_WAIT_MS);
+
+			// send message to the reciever
 		})
 		.subscribe();
 
