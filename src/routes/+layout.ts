18baca1feb57c0bec96927a275bae5afeaadc2bc
B-cabrera <brendenc29@gmail.com>
2024-04-06 19:25:12 -0400

    feat: add message store in layout

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,16 +3,20 @@
 /* eslint-disable import/prefer-default-export */
 
 import { createBrowserClient } from '@supabase/ssr';
+import { writable, type Writable } from 'svelte/store';
+import type { Message } from '@prisma/client';
 import type { LayoutLoad } from './$types';
 import { PUBLIC_SUPABASE_ANON, PUBLIC_SUPABASE_URL } from '$env/static/public';
 
 export const load = (async ({ data }) => {
 	const supabaseBrowserClient = createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON);
 	const userChannel = supabaseBrowserClient.channel(data.userID!).subscribe();
+	const messageStore: Writable<[string, Message[]][]> = writable();
 
 	return {
 		...data,
 		supabaseBrowserClient,
 		userChannel,
+		messageStore,
 	};
 }) satisfies LayoutLoad;
