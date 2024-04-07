a26b719ff6879e5e8a17019c317b87fd579bc958
B-cabrera <brendenc29@gmail.com>
2024-04-06 22:07:30 -0400

    feat: add scroll to bottom for message feed

--- src/lib/components/MessageFeedBlock.svelte
+++ src/lib/components/MessageFeedBlock.svelte
@@ -12,11 +12,27 @@
 	export let otherUserName: string | undefined;
 	export let currentUserID: string;
 
+	let feedElem: HTMLElement;
+
+	function scrollChatBottom(): void {
+		if (!feedElem) return;
+
+		requestAnimationFrame(() => {
+			// Scroll to the bottom
+			feedElem.scrollTo({
+				top: feedElem.scrollHeight,
+				behavior: 'instant',
+			});
+		});
+	}
+
+	$: messageFeed, scrollChatBottom();
+
 	$: senderName = otherUserName!;
 </script>
 
 <div class=" p-4 overflow-y-auto h-full">
-	<section class="w-full max-h-[82vh] overflow-y-auto space-y-4">
+	<section bind:this={feedElem} class="w-full max-h-[82vh] overflow-y-auto space-y-4">
 		{#each messageFeed as chat}
 			<MessageRow
 				{senderName}
