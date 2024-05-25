6eed411e1cfeadf4164a22a53f5e3b98731d42af
B-cabrera <brendenc29@gmail.com>
2024-05-24 22:53:47 -0400

    feat: add debounce mark message as read func

--- src/lib/components/MessageFeedBlock.svelte
+++ src/lib/components/MessageFeedBlock.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import type { MessageWithoutID } from '../../routes/+layout.server';
 	import MessageRow from './MessageRow.svelte';
 
@@ -6,7 +7,17 @@
 	export let otherUserName: string | undefined;
 	export let currentUserID: string;
 
+	const DEBOUNCE_MS = 1000;
+
 	let feedElem: HTMLElement;
+	let unreadMessages: Set<{
+		conversationId: string;
+		timeSent: Date;
+		content: string;
+	}> = new Set();
+	let debounceTimer: NodeJS.Timeout | null = null;
+	let totalRetries = 0;
+	let MAX_RETRIES = 3;
 
 	function scrollChatBottom(): void {
 		if (!feedElem) return;
@@ -23,6 +34,63 @@
 	$: messageFeed, scrollChatBottom();
 
 	$: senderName = otherUserName!;
+
+	$: {
+		const _ = messageFeed;
+
+		browser && handleDebounce();
+	}
+
+	function handleDebounce() {
+		console.log('here');
+
+		if (debounceTimer) {
+			clearTimeout(debounceTimer);
+		}
+
+		messageFeed.forEach((message) => {
+			if (message.senderId != currentUserID && !message.read) {
+				message.read = true;
+
+				unreadMessages.add({
+					conversationId: message.conversationId,
+					timeSent: message.timeSent,
+					content: message.content,
+				});
+			}
+		});
+
+		if (!unreadMessages.size) return;
+
+		debounceTimer = setTimeout(markMessagesAsRead, DEBOUNCE_MS);
+
+		async function markMessagesAsRead() {
+			if (unreadMessages.size > 0) {
+				const messagesSetAsArray = Array.from(unreadMessages);
+
+				const result = await fetch(`/api/chats/${messagesSetAsArray[0].conversationId}/read`, {
+					method: 'PATCH',
+					body: JSON.stringify(messagesSetAsArray),
+				});
+
+				if (!result.ok && totalRetries < MAX_RETRIES) {
+					const message = await result.json();
+
+					// just a delay
+					await new Promise((resolve) => setTimeout(resolve, 1000));
+
+					totalRetries++;
+
+					await markMessagesAsRead();
+
+					return;
+				}
+
+				unreadMessages.clear();
+				debounceTimer = null;
+			}
+		}
+	}
 </script>
 
 <div class=" p-4 overflow-y-auto h-full">
