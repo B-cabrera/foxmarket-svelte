97238fe4c84cfe4e2a8e0f942a93482325a366b6
B-cabrera <brendenc29@gmail.com>
2024-05-30 16:18:38 -0400

    feat: add brand filter and dropdown to feed

--- src/routes/feed/+page.svelte
+++ src/routes/feed/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import BrandFilterBlock from '$lib/components/BrandFilterBlock.svelte';
 	import Dropdown from '$lib/components/Dropdown.svelte';
 	import Listing from '$lib/components/Listing.svelte';
 	import PriceFilterBlock from '$lib/components/PriceFilterBlock.svelte';
@@ -6,15 +7,20 @@
 
 	export let data: PageData;
 	const userID = data.userID!;
+	const { brandSet } = data;
 
 	let minPrice = '';
 	let maxPrice = '';
+	let chosenBrands: string[] = [];
 </script>
 
 <div id="feed" class="h-[calc(100vh-56px)] flex">
 	<div id="filters" class="w-1/6">
 		<Dropdown label={'Price'}>
 			<PriceFilterBlock bind:min={minPrice} bind:max={maxPrice} />
+		</Dropdown>
+		<Dropdown label={'Brand'}>
+			<BrandFilterBlock bind:chosenBrands brands={brandSet} />
 		</Dropdown>
 	</div>
 	<div id="items" class="w-full grid grid-cols-4 gap-8 px-5">
