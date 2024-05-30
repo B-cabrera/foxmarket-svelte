ef8f9529b9416dc82bac800641b0de922aa0b0ad
B-cabrera <brendenc29@gmail.com>
2024-05-30 14:49:05 -0400

    feat: create dropdown component

---
+++ src/lib/components/Dropdown.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let label: string;
+
+	let showMenu = false;
+</script>
+
+<div class="text-slate-50 transition-all">
+	<hr />
+	<button on:click={() => (showMenu = !showMenu)} class="w-full flex p-3 justify-between">
+		<p>{label}</p>
+		{#if showMenu}
+			<span class="material-symbols-outlined"> keyboard_arrow_up </span>
+		{:else}
+			<span class="material-symbols-outlined"> keyboard_arrow_down </span>
+		{/if}
+	</button>
+
+	{#if showMenu}
+		<slot />
+	{/if}
+	<hr />
+</div>
