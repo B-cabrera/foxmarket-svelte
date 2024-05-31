375e51c63e4c83b030db487e9ab6d778468080af
B-cabrera <brendenc29@gmail.com>
2024-05-31 15:10:35 -0400

    feat: add filter obj generation from search params on items endpoint

--- src/routes/api/items/+server.ts
+++ src/routes/api/items/+server.ts
@@ -4,10 +4,50 @@
 
 import { json, type RequestHandler } from '@sveltejs/kit';
 import prisma from '$lib/utils/prismaClient';
+import type { Prisma } from '@prisma/client';
 
-export const GET: RequestHandler = async ({ locals }) => {
+function generatePrismaFilterObjsFromFilters(params: URLSearchParams) {
+	let filterObjs: Prisma.ListingWhereInput[] = [];
+	let keyList = Array.from(params.keys());
+	let addedKeys: string[] = [];
+
+	keyList.forEach((key) => {
+		if (addedKeys.includes(key)) return;
+
+		addedKeys.push(key);
+		const input: Prisma.ListingWhereInput = key == 'price' ? { AND: [] } : { OR: [] };
+		const inputList = input.AND! as Prisma.ListingWhereInput[] ?? input.OR!;
+
+		params.getAll(key).forEach((value) => {
+			const keyAsValidIndex = key as 'location' | 'price' | 'brand' | 'size';
+			const entry: Prisma.ListingWhereInput = {};
+
+			if (keyAsValidIndex == 'price') {
+				const operation = value.split('e');
+				const comparisonObj: Prisma.DecimalFilter = operation[0] == 'gt' ? { gte: operation[1] } : { lte: operation[1] };
+
+				entry[keyAsValidIndex] = comparisonObj;
+			} else {
+				entry[keyAsValidIndex] = value;
+			}
+
+			inputList.push(entry);
+		});
+
+		filterObjs.push(input);
+	});
+
+	return filterObjs;
+}
+
+export const GET: RequestHandler = async ({ locals, url }) => {
+	const filterObjs = generatePrismaFilterObjsFromFilters(url.searchParams);
 	const allListings = await prisma.listing.findMany({
-		where: { sold: false },
+		where: {
+			AND: [{ sold: false },
+			...filterObjs
+			]
+		},
 		include: {
 			favoritedBy: true,
 		},
