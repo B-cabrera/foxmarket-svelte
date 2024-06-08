064b3a59a4d8690cc9717598b960b68b6afd692a
B-cabrera <brendenc29@gmail.com>
2024-06-08 16:54:17 -0400

    feat: add update query to mark item as sold in db

--- src/routes/api/item/[item_id]/sell/+server.ts
+++ src/routes/api/item/[item_id]/sell/+server.ts
@@ -1,12 +1,27 @@
 import prisma from "$lib/utils/prismaClient";
 import { json, type RequestHandler } from "@sveltejs/kit";
 
-export const PATCH: RequestHandler = async ({ request }) => {
+export const PATCH: RequestHandler = async ({ request, params }) => {
+	// will use the buyer information in ratings, not rn though 
 	const data = await request.json();
+	const { item_id } = params;
 
-	// mark the item as sold here
-	console.log({ data });
+	try {
+		await prisma.listing.update({
+			where: {
+				id: item_id
+			},
+			data: {
+				sold: true
+			}
+		});
 
-	return json({});
+		return json({ message: "Item marked as sold" });
+	} catch (error) {
+		const cleanError = error as Error;
+
+		return new Response(JSON.stringify({ message: cleanError.message }), { status: 500 })
+	}
+
 };
 
