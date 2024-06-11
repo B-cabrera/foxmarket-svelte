f3926fd56bd2f4e26200ad1eb5f163b72adc7d4a
B-cabrera <brendenc29@gmail.com>
2024-06-11 00:35:18 -0400

    feat: add transaction create query when marking item as sold

--- src/routes/api/item/[item_id]/sell/+server.ts
+++ src/routes/api/item/[item_id]/sell/+server.ts
@@ -1,7 +1,7 @@
 import prisma from "$lib/utils/prismaClient";
 import { json, type RequestHandler } from "@sveltejs/kit";
 
-export const PATCH: RequestHandler = async ({ request, params }) => {
+export const PATCH: RequestHandler = async ({ request, params, locals }) => {
 	// will use the buyer information in ratings, not rn though 
 	const data = await request.json();
 	const { item_id } = params;
@@ -16,7 +16,16 @@ export const PATCH: RequestHandler = async ({ request, params }) => {
 			}
 		});
 
-		return json({ message: "Item marked as sold" });
+		const transaction = await prisma.transaction.create({
+			data: {
+				sellerId: locals.data!.userID!,
+				buyerId: data.buyerID,
+				itemId: item_id!,
+				time: new Date(Date.now())
+			}
+		})
+
+		return json({ message: "Item marked as sold", transactionID: transaction.id });
 	} catch (error) {
 		const cleanError = error as Error;
 
