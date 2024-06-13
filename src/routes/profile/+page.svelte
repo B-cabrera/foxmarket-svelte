dd9465fccdb1a84f4b81da737ad470f75f272b8e
B-cabrera <brendenc29@gmail.com>
2024-06-12 22:51:30 -0400

    feat: add rating display

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,15 +1,38 @@
 <script lang="ts">
+	import { Ratings } from '@skeletonlabs/skeleton';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 
-	const { username } = data;
+	const { username, rating } = data;
 </script>
 
 <div class="w-full flex justify-center h-[calc(100vh-56px)]">
 	<div class="flex flex-col w-2/3">
-		<div class="mt-10 w-full">
+		<div class="flex mt-10 w-full gap-5 justify-center">
 			<h1 class="text-2xl text-slate-50 tracking-wider rounded border w-min p-4">{username}</h1>
+
+			{#if rating}
+				<Ratings value={rating / 1} max={5} class="w-min !text-slate-50">
+					<svelte:fragment slot="empty">
+						<span class="material-symbols-outlined text-7xl font-light reg_symbol"> star </span>
+					</svelte:fragment>
+					<svelte:fragment slot="half">
+						<span class="material-symbols-outlined text-7xl font-light reg_symbol">
+							star_half
+						</span>
+					</svelte:fragment>
+					<svelte:fragment slot="full">
+						<span class="material-symbols-outlined text-7xl font-light fill_symbol"> star </span>
+					</svelte:fragment>
+				</Ratings>
+			{:else}
+				<div class="flex items-center">
+					<h1 class="text-2xl text-slate-50 tracking-wider">
+						No rating yet...
+					</h1>
+				</div>
+			{/if}
 		</div>
 	</div>
 </div>
