c03de695ed929bcf7e0f2db1dceef9df38936f77
B-cabrera <brendenc29@gmail.com>
2024-06-12 22:37:05 -0400

    feat: add username displayed on page

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	const { username } = data;
 </script>
 
-<div class="">
-	<p>Profile page here</p>
+<div class="w-full flex justify-center h-[calc(100vh-56px)]">
+	<div class="flex flex-col w-2/3">
+		<div class="mt-10 w-full">
+			<h1 class="text-2xl text-slate-50 tracking-wider rounded border w-min p-4">{username}</h1>
+		</div>
+	</div>
 </div>
