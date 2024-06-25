404bbf72bf0e7e53b5912b846d4cceaa23d1703e
B-cabrera <brendenc29@gmail.com>
2024-06-25 12:16:00 -0400

    feat: add prisma query to get all unrated transactions

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -4,6 +4,7 @@
 
 import type { Listing } from '@prisma/client';
 import type { PageServerLoad } from './$types';
+import prisma from '$lib/utils/prismaClient';
 
 export const load: PageServerLoad = async ({ locals, fetch }) => {
 	const username = locals.data!.username!;
@@ -16,10 +17,35 @@ export const load: PageServerLoad = async ({ locals, fetch }) => {
 	const data = await fetch(`/api/items/${userID}`);
 	const { listings } = await data.json();
 
+	// one time thing ig, i dont need a route for unrated transaction
+	const unratedTransactions = await prisma.transaction.findMany({
+		where: {
+			OR: [
+				{
+					AND: [
+						{ sellerId: userID },
+						{ hasSellerRated: false }
+					]
+				},
+				{
+					AND: [
+						{ buyerId: userID },
+						{ hasBuyerRated: false }
+					]
+				}
+			],
+		},
+		include: {
+			buyer: true,
+			seller: true
+		}
+	});
+
 	return {
 		username,
 		userID,
 		rating: rating as number | null,
-		listings: listings as Listing[]
+		listings: listings as Listing[],
+		unratedTransactions
 	}
 };
