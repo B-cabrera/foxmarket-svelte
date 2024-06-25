fb098345e301c9bcfa6a57ac96fbf9c895248c2c
B-cabrera <brendenc29@gmail.com>
2024-06-25 12:17:44 -0400

    feat: add basic display component for unrated transactions

---
+++ src/lib/components/TransactionDisplay.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { Prisma } from '@prisma/client';
+
+	export let transaction: Prisma.TransactionGetPayload<{
+		include: {
+			buyer: true;
+			seller: true;
+		};
+	}>;
+	export let currentUserID: string;
+</script>
+
+<div class="w-5/6 text-slate-50 border p-4 flex justify-around items-center">
+	<p class="text-sm">
+		You {currentUserID == transaction.buyerId
+			? `bought from "${transaction.seller.username}"`
+			: `sold to "${transaction.buyer.username}"`}
+		on {transaction.time.toDateString()}
+	</p>
+
+	<button class="btn">Rate</button>
+</div>
