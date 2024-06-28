57fa50b638b96f002b71d4d13e5b32c3623a5c3b
B-cabrera <brendenc29@gmail.com>
2024-06-27 23:50:31 -0400

    feat: add rating display to allow rates

--- src/lib/components/TransactionDisplay.svelte
+++ src/lib/components/TransactionDisplay.svelte
@@ -13,17 +13,28 @@
 		};
 	}>;
 	export let currentUserID: string;
+
+	let allowRating = false;
 </script>
 
 <div class="w-5/6 text-slate-50 border p-4 flex justify-around items-center">
-	<img src={transaction.item.imageUrl} alt="" class="w-1/6" />
+	{#if allowRating}
+		<p>RATING HERE</p>
 
-	<p class="text-sm">
-		You {currentUserID == transaction.buyerId
-			? `bought from "${transaction.seller.username}"`
-			: `sold to "${transaction.buyer.username}"`}
-		on {transaction.time.toDateString()}
-	</p>
+		<button class="btn bg-maristred">Submit</button>
+		<button class="btn bg-maristgrey text-slate-950" on:click={() => (allowRating = false)}
+			>Cancel</button
+		>
+	{:else}
+		<img src={transaction.item.imageUrl} alt="" class="w-1/6" />
 
-	<button class="btn bg-maristred">Rate</button>
+		<p class="text-sm">
+			You {currentUserID == transaction.buyerId
+				? `bought from "${transaction.seller.username}"`
+				: `sold to "${transaction.buyer.username}"`}
+			on {transaction.time.toDateString()}
+		</p>
+
+		<button class="btn bg-maristred" on:click={() => (allowRating = true)}>Rate</button>
+	{/if}
 </div>
